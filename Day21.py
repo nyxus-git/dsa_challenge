c4bef5a5c063fcb179d70a7b316de09b313a007c
nyxus <91864739+nyxus-git@users.noreply.github.com>
2025-09-04 19:55:09 +0530

    Create Day21.py

---
+++ Day21.py
@@ -0,0 +1,14 @@
+def insert_at_bottom(stack, item):
+    if not stack:
+        stack.append(item)
+    else:
+        top = stack.pop()
+        insert_at_bottom(stack, item)
+        stack.append(top)
+
+def reverse_stack(stack):
+    if not stack:
+        return
+    top = stack.pop()
+    reverse_stack(stack)
+    insert_at_bottom(stack, top)
